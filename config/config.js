2ec871a9e51946debe2aa6ec7abec2895c7885f3
AndrewSC208 <meilingandrew@gmail.com>
2016-08-08 11:49:10 -0700

    Created Procfile, updated environment => production, updated port for heroku

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const milieu = require('milieu');
 const config = milieu('port4-api-test', {
   environment: 'production',
   server: {
-    port: 8080
+    port: (process.env.PORT || 5000)
   },
   mongo: {
     url: 'mongodb://test:toTheMoon@ds145385.mlab.com:45385/port4-messages'
