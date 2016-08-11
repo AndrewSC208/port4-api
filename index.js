c73c3acfe5fa2ef7f552e0036959923add137c6a
AndrewSC208 <meilingandrew@gmail.com>
2016-08-10 22:18:36 -0700

    added middleware to allow Access controll methods

--- index.js
+++ index.js
@@ -16,8 +16,28 @@ require('./libraries/promisify-all')(['mongoose']);
 
 mongoose.connect(config.mongo.url);
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://mighty-journey-67013.herokuapp.com/pet');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+})
+
 app.use(helmet());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
